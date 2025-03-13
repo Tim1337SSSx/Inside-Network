0b22eefb3530f1b4e7c7a8a71cd50e134e26c585
♡𝐌𝐨𝐭𝐢𝐟𝐲♡ <tim657520@gmail.com>
2025-03-13 20:14:16 +0300

    Add files via upload

---
+++ webmanifest.php
@@ -0,0 +1,33 @@
+{
+    "name": "INSIDE CLUB",
+    "icons": [
+        {
+            "src": "\/data\/assets\/logo\/192.png",
+            "sizes": "192x192",
+            "purpose": "any"
+        },
+        {
+            "src": "\/data\/assets\/logo\/192.png",
+            "sizes": "192x192",
+            "purpose": "maskable"
+        },
+        {
+            "src": "\/data\/assets\/logo\/512.png",
+            "sizes": "512x512",
+            "purpose": "any"
+        },
+        {
+            "src": "\/data\/assets\/logo\/512.png",
+            "sizes": "512x512",
+            "purpose": "maskable"
+        }
+    ],
+    "lang": "ru-RU",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/",
+    "id": "\/?_pwa=1",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#121212",
+    "theme_color": "#ffffff"
+}
